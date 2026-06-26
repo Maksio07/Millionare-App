c09ee155d29f3f78d30a5beb56cc609f3d51f2d1
Maks <mmaksimov20000806@gmail.com>
2026-06-26 15:16:14 +0200

    changed

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,5 +1,17 @@
 'use client'
 
 export default function Game() {
-	return <></>
+	return (
+		<div
+			style={{
+				background: '#02021a',
+				minHeight: '100vh',
+				color: '#fff',
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+			}}>
+			<p>Inicjalizacja rozgrywki...</p>
+		</div>
+	)
 }
